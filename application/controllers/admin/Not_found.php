5538110f17609b23008d77073b91ae3d12ed7c27
faisalharis <faisalhaaaaaris@gmail.com>
2020-08-24 17:05:32 +0700

    tambahan model

---
+++ application/controllers/admin/Not_found.php
@@ -0,0 +1,15 @@
+<?php
+
+class Not_found extends CI_Controller {
+    public function __construct()
+    {
+		parent::__construct();
+	}
+
+	public function index()
+	{
+        // load view admin/overview.php
+        $this->output->set_status_header('404');
+        $this->load->view("admin/not_found");
+	}
+}
